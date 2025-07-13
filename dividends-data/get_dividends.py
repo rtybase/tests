4162aaf0e3d252d4aa0e4e4148c3b99ab4eefc7f
rtybase <ruslan.ciurca@gmail.com>
2025-07-13 12:50:30 +0100

    Made the script less verbose

--- dividends-data/get_dividends.py
+++ dividends-data/get_dividends.py
@@ -3,12 +3,14 @@ import json
 import sys
 import time
 
+PRINT_DIVIDENDS_ONLY = True
+
 def get_data(url):
     req = urllib.request.Request(
         url,
         data = None,
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0',
             'Accept': '*/*',
             'DNT': "1",
             'Origin': 'https://www.nasdaq.com/',
@@ -24,10 +26,10 @@ def get_dividends_for(symbol):
     dividends = get_data("https://api.nasdaq.com/api/quote/{0}/dividends?assetclass=stocks".format(symbol))
     stock = get_data("https://api.nasdaq.com/api/quote/{0}/info?assetclass=stocks".format(symbol))
     if ("data" in dividends) and (dividends["data"] is not None):
-        print("%s,%s,$%s,%s" % (stock["data"]["symbol"], stock["data"]["primaryData"]["lastSalePrice"], dividends["data"]["annualizedDividend"], dividends["data"]["yield"]), flush = True)
-    elif ("data" in stock) and (stock["data"] is not None):
-        print("%s,%s,," % (stock["data"]["symbol"], stock["data"]["primaryData"]["lastSalePrice"]), flush = True)
-    else:
+        print("%s,\"%s\",$%s,%s" % (stock["data"]["symbol"], stock["data"]["primaryData"]["lastSalePrice"], dividends["data"]["annualizedDividend"], dividends["data"]["yield"]), flush = True)
+    elif ("data" in stock) and (stock["data"] is not None) and (not PRINT_DIVIDENDS_ONLY):
+        print("%s,\"%s\",," % (stock["data"]["symbol"], stock["data"]["primaryData"]["lastSalePrice"]), flush = True)
+    elif (not PRINT_DIVIDENDS_ONLY):
         print("%s,,," % (symbol), flush = True)
 
 def get_file_content(file):
